89b990bd0aa12b2741d136d272aa500d8a1927f4
alexander.Khatamov <alexk2120@gmail.com>
2021-12-20 16:41:52 -0500

    commit tree f2da771fcf8939036b2adc08762364cd11ada1d1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+def name=(name_in)
+    @name = name_in
+end    
+
+def name
+    @name
+end
+
+def breed=(breed_in)
+    @breed = breed_in
+end
+
+def breed
+    @breed
+end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+    def name=(name_in)
+            @name = name_in
+  end    
+        
+     def name
+            @name
+    end
+        
+        def job=(job_in)
+            @job = job_in
+        end
+        
+        def job
+            @job
+        end
+        
+        
 end
